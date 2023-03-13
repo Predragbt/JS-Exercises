338116c41c6483c655f80bcb21574bca74f0e8b7
Predragbt <127343455+Predragbt@users.noreply.github.com>
2023-03-13 18:27:09 +0100

    Update exercises.js

--- JS-Exercises/exercises.js
+++ JS-Exercises/exercises.js
@@ -1,5 +1,5 @@
-// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
-// 2. Log their values to the console
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions).
+// 2. Log their values to the console.
 
 /*
 let country = 'Macedonia';
@@ -10,8 +10,8 @@ console.log(continent);
 console.log(population);
 */
 
-// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
-// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet.
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console.
 
 /*
 let isIsland = false;
